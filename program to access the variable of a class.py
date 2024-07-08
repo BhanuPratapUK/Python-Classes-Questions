41bd32814d09dbb3a2ac148a9cfeb9e7e026beae
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-07-08 09:58:20 +0100

    Adding

---
+++ program to access the variable of a class.py
@@ -0,0 +1,11 @@
+class vfind:
+    var=10
+object1 = vfind()
+print('Here is the class variable-->',object1.var)
+
+'''I just made a class and given the name as "vfind" and then I made a class variable name as "var"  then
+I  created an Object to access the class variable  and you see I used the syntax to access the class
+variable with the help  dot(.) operator ----> (OBJECT.CLASS VARIABLE) and in classes we can access the 
+class variable + class methods(FUNCTIONS) also and after combining them we can say they are "CLASS MEMBERS"
+
+'''
