2fcf95e1be7edd6e3421cb3d0bedd97d89b4ece0
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-07-08 10:13:45 +0100

    Adding

---
+++ Progra to access class members using the class object.py
@@ -0,0 +1,7 @@
+class ABC():
+    var = 100
+    def show(self):
+        print("Hey how are you I am inside of this class Methods")
+display = ABC()
+print('MY value is this-->',display.var)
+display.show()
