6bdfc35c51df3b0f1c7e582e0c20d5a2805b53d3
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-07-26 20:29:58 +0100

    Adding

--- SingleNumber.py
+++ SingleNumber.py
@@ -40,16 +40,5 @@ print(x.new2)
 del x.new1
 print(x.new2)
 print(x.new1)
-(11 Question)class ABC:
-    def __init__(self,val):
-        self.val= val
-    def display(self):
-        print('The value is --<>',self.val)
-x= ABC(1000)
-x.display()
-print('Checking the value is present or not-->',hasattr(x,'val'))
-getattr(x,'val')
-setattr(x,'val',12)
-print('The value is present in or not-->',x.val)
-delattr(x,'val')'''
+
 
