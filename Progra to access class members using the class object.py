307a2a55f42da24df4322db82fbb7b8743869b13
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-09-28 23:51:13 +0100

    Adding

--- Progra to access class members using the class object.py
+++ Progra to access class members using the class object.py
@@ -1,7 +1,7 @@
 class ABC():
     var = 100
-    def show(self):
+    def Learn(self):
         print("Hey how are you I am inside of this class Methods")
 display = ABC()
 print('The value of the var is-->',display.var)
-display.show()
+display.learn()
